5e46a896948fe720884d67b5819a1fcf8802d82c
ivyd <iivy.dding@gmail.com>
2022-02-02 12:02:59 -0500

    update home page buttons

--- sortalgorithgame/src/components/Home.js
+++ sortalgorithgame/src/components/Home.js
@@ -3,21 +3,16 @@ import '../styles/Home.css';
 
 export default function Home() {
     return (
-        <div >
+        <div class ="center">
             <br></br>
             <div className="ChooseAlgo" >Choose a Sorting Algorithm</div>
             <br></br>
             <a href="/mergeSortLevels"><button className="btnDiv">Merge Sort</button></a>
             <br></br>
-            <a href="/levelTwo"><button className="btnDiv">Level 2</button></a>
+            <button className="btnDiv">Another Sort</button>
             <br></br>
-            <button className="btnDiv">Level 3</button>
-            <br></br>
-            <button className="btnDiv">Level 4</button>
-            <br></br>
-            <button className="btnDiv">Level 5</button>
-            <br></br>
-            <button className="btnDiv">Custom Level</button>
+            <button className="btnDiv">...</button>
+        
 
         </div>
 
