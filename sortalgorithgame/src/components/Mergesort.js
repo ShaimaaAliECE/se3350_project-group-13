60ac7c85424ba01ec6bb4646c1ba22d68e3f635d
sabuazma <sabuazma@uwo.ca>
2022-02-03 14:05:38 -0500

    next step logic (needs improvement)

--- sortalgorithgame/src/components/Mergesort.js
+++ sortalgorithgame/src/components/Mergesort.js
@@ -73,7 +73,7 @@ export default function Mergesort() {
         // Left and right child of array 
         let arrayThreeLeft = arrayThree.left;
         let arrayThreeRight = arrayThree.right;
-
+        if(steps==1){
         return (
             <div className="visual-container">
                 <div className="split-container">
@@ -140,7 +140,7 @@ export default function Mergesort() {
                     </div>
                 </div>
             </div>
-        );
+        );}
     }
 
     // Merge and sort two arrays
@@ -177,7 +177,7 @@ export default function Mergesort() {
         let mergeSortedArrayTwo = mergeSort(arrayTwoLeft, arrayTwoRight);
         // Fully sorted array
         let mergeSortedArrayOne = mergeSort(arrayOneLeft, arrayOneRight);
-
+        if(steps==2){
         return (
             <div className="sort-container">
                 { array.length < 5 ?
@@ -217,13 +217,15 @@ export default function Mergesort() {
                     })}
                 </div>
             </div>
-        )
+        )}
     }
 
     return (
         <div className="mergesort-container">
             <br></br>
-            <button className="nxtBtnDiv">next step</button>
+            <button className="nxtBtnDiv" onClick={()=>{
+                steps == 0 ? setSteps(1): setSteps(2);    
+            }}>next step</button>
             <br></br>
             {Array(array)}
             {DisplaySplit(array)}
