ee189b4474805370a002e19c280fc59622ba662b
HerbertShin <hshin63@uwo.ca>
2022-02-10 14:51:40 -0500

    frontend login with persistence

--- sortalgorithgame/src/components/Login.js
+++ sortalgorithgame/src/components/Login.js
@@ -1,15 +1,53 @@
-import React from "react";
+import {React, useState} from "react";
 import '../styles/Home.css';
+//import bcrypt from 'bcrypt';
 
 export default function Login() {
+    // States for username and password
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    // Authenticate function
+    function authenticate(username, password){
+        // Can sub in code to check with db later 
+        if(username === "admin" && password === "admin"){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    // Login function 
+    async function login(e){
+        //e.preventDefault();
+        const auth = await authenticate(username, password);
+        if(auth === true){
+            localStorage.setItem('Username', username);
+            setUsername("");
+            setPassword("");
+        } else{
+            alert("Incorrect username/password");
+        }
+    }
+
+    // Text onChange username function 
+    function onChangeUsername(e){
+        setUsername(e.target.value);
+    }
+
+    // Text onChange password function 
+    function onChangePassword(e){
+        setPassword(e.target.value);
+    }
+
     return (
         <div className="center">
             <form action="">
             <label>Username</label><br></br>
-                <input type="text" id="username" name="username"></input><br></br>
+                <input onChange={onChangeUsername} type="text" id="username" name="username"></input><br></br>
                 <label>Password</label><br></br>
-                <input type="text" id="password" name="password"></input><br></br>
-                <input type="submit" value="Login"></input>
+                <input onChange={onChangePassword} type="password" id="password" name="password"></input><br></br>
+                <button onClick={login} type="submit">Login</button>
             </form>
         </div>
 
