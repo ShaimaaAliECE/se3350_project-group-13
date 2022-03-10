936415fcd18aa8d1304037f44e59284f0bb81595
Tyler <tmohaupt@uwo.ca>
2022-03-10 18:16:51 -0500

    reformat

--- sortalgorithgame/src/components/CustomPage.js
+++ sortalgorithgame/src/components/CustomPage.js
@@ -5,21 +5,21 @@ import Mergesort_input from "./Mergesort_input";
 const input = [];
 let i;
 
-function CustomPage() {
-    function getInputs() {
-        let inputs = document.querySelectorAll("input");
-        for (i of inputs){
-            input[i] = inputs;
-        }
-        //Mergesort_input();
+function getInputs() {
+    let inputs = document.querySelectorAll("input");
+    for (i of inputs){
+        input[i] = inputs;
     }
+    //Mergesort_input();
+}
 
+function CustomPage() {    
     return (
-      <div>
-          <br></br>
+      <div className = 'center'>
           <div><label className = 'levelLabels'>Minimum Value:  </label><input type = 'text' id = 'min' className = ''></input></div>
           <div><label className = 'levelLabels'>Maximum Value:  </label><input type = 'text' id = 'max' className = ''></input></div>
-          <div><label className = 'levelLabels'>Number of elements:  </label><input type = 'text' id = 'max' className = ''></input></div>
+          <div><label className = 'levelLabels'>Number of elements:  </label><input type = 'text' id = 'num' className = ''></input></div>
+          <br></br>
           <button className="submitBtn" id="checkbtn" onClick={getInputs()}>Begin!</button>
       </div>
     );
