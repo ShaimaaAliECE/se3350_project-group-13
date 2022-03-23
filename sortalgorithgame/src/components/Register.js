24624a89733692eb7041bcc15972584ac1b407a5
sabuazma <sabuazma@uwo.ca>
2022-03-23 12:21:07 -0400

    login register changes

--- sortalgorithgame/src/components/Register.js
+++ sortalgorithgame/src/components/Register.js
@@ -1,19 +1,56 @@
 import React from "react";
-import '../styles/Login.css';
+import axios from "utils/axios";
+import '../styles/Register.css';
 
-export default function Register() {
-    return (
-        <div className="login-container">
-            <div className="login-window">
+class Register extends React.Component{
+    state = {
+        id: "",
+        username: "",
+        pass: "",
+        email: "",
+        failedRegistser: false
+    };
+
+    handleChange = (event) => {
+
+    }
+
+    handleRegister = (event) => {
+
+    }
+
+    render() {
+        return(
+            <div className="register-container">
+            <div className="register-window">
             <h1>Register</h1>
                 <form action="">
-                    <input className = "login-input" type="text" id="email" name="email" placeholder="Email"></input><br></br>
-                    <input className = "login-input" type="text" id="username" name="username" placeholder="Username"></input><br></br>
-                    <input className = "login-input" type="password" id="password" name="password" placeholder="Password"></input><br></br>
-                    <input className = "login-input" type="submit" value="Register"></input>
+                    <input className = "register-input" type="text" id="email" name="email" placeholder="Email"></input><br></br>
+                    <input className = "register-input" type="text" id="username" name="username" placeholder="Username"></input><br></br>
+                    <input className = "register-input" type="password" id="password" name="password" placeholder="Password"></input><br></br>
+                    <input className = "register-input" type="submit" value="Register"></input>
+                </form>
+            </div>
+            </div>
+        );
+    }
+}
+
+export default Register;
+/**
+export default function Register() {
+    return (
+        <div className="register-container">
+            <div className="register-window">
+            <h1>Register</h1>
+                <form action="">
+                    <input className = "register-input" type="text" id="email" name="email" placeholder="Email"></input><br></br>
+                    <input className = "register-input" type="text" id="username" name="username" placeholder="Username"></input><br></br>
+                    <input className = "register-input" type="password" id="password" name="password" placeholder="Password"></input><br></br>
+                    <input className = "register-input" type="submit" value="Register"></input>
                 </form>
             </div>
         </div>
 
     )
-}
+} */
