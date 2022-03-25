f8bca1c3dee4754278412fce991402f467973455
sabuazma <sabuazma@uwo.ca>
2022-03-25 17:50:25 -0400

    profile - setting states

--- sortalgorithgame/src/components/Profile.js
+++ sortalgorithgame/src/components/Profile.js
@@ -1,15 +1,39 @@
 import React from "react";
 import axios from "../utils/axios";
-import '../styles/Home.css';
+import '../styles/Profile.css';
 
 class Profile extends React.Component {
     constructor(props) {
-        
+        super(props);
+        this.state = {
+            username: localStorage.getItem('Username'),
+            levelOne: [],
+            levelTwo: [],
+            levelThree: [],
+            levelFour: [],
+            levelFive: [],
+            customLevel: []
+        }
+        //level one data
+        axios.get(`http://localhost:3001/getLevelOneInfo?username=${this.state.username}`).then((res) => {
+            this.setState({levelOne: res.data});
+        });
+        // level 2-5, and custom data 
+        this.handleLevelData("LevelTwo", this.state.levelTwo);
+        this.handleLevelData("LevelThree", this.state.levelThree);
+        this.handleLevelData("LevelFour", this.state.levelFour);
+        this.handleLevelData("LevelFive", this.state.levelFive);
+        this.handleLevelData("CustomLevel", this.state.customLevel);
+    }
+    handleLevelData(level, stateLevel) {
+        axios.get(`http://localhost:3001/getLevelInfo?username=${this.state.username}&level=${level}`).then((res) => {
+            this.setState({stateLevel: res.data});
+        });
     }
     render() {
         return(
             <div>
-                <h1>Blank profile page</h1>
+                <h1>Profile</h1>
             </div>
         );
     }
